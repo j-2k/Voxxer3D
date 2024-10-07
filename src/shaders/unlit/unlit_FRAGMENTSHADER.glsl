340900c1f05d1af061fe07f7e541033786bf3d1d
Juma Al Remeithi <juma_ad@hotmail.com>
2024-10-07 08:09:54 +0400

    cleaning shaders

--- src/shaders/unlit/unlit_FRAGMENTSHADER.glsl
+++ src/shaders/unlit/unlit_FRAGMENTSHADER.glsl
@@ -2,40 +2,9 @@ precision mediump float;
 
 varying vec2 v_color;           // Get the color from the vertex shader
 
-//uniform vec4 u_color;           // The color uniform (UV)
 uniform sampler2D u_texture;    // The texture uniform
-void main() {
-    //gl_FragColor = u_color;
 
-    //gl_FragColor = vec4(v_color,0.0,1.0);
-    
+void main() {
     vec2 uv = v_color;
-    gl_FragColor = texture2D(u_texture, uv);
+    gl_FragColor = texture2D(u_texture, v_color);
 }
-
-
-
-
-/*
-//WebGL 2.0 Shader Implementation
-#version 300 es
-precision mediump float;
-
-// Uniforms
-uniform vec4 u_color;  // The color uniform
-
-// Output fragment color
-out vec4 outColor;
-
-void main() {
-    // Set the output color to the uniform color
-    outColor = u_color;
-}
-
-//WebGL 1.0 Shader Implementation
-precision mediump float;
-uniform vec4 u_color;
-void main() {
-    gl_FragColor = u_color;
-}
-*/
--- src/shaders/unlit/unlit_VERTEXSHADER.glsl
+++ src/shaders/unlit/unlit_VERTEXSHADER.glsl
@@ -4,56 +4,14 @@ attribute vec2 a_color;     // Attribute to hold the color of the vertex
 varying vec2 v_color;       // Pass the color to the fragment shader
 
 uniform mat4 u_modelMatrix; // Model transformation matrix
-uniform vec2 u_resolution;  // Resolution of the canvas to normalize the position if canvas size changes/is not 1:1
+//uniform vec2 u_resolution;  // Resolution of the canvas to normalize the position if canvas size changes/is not 1:1
 
 void main() {
     
     v_color = a_color;
-
-    //vec2 scaledAspectRatio = a_position.xy * vec2(u_resolution.y / u_resolution.x,1.0);
-    //gl_Position = u_modelMatrix * vec4(scaledAspectRatio.xy,a_position.z, 1.0);
-
 
     //gl_pos excepts clip space, which is from -1 to 1 in x and y
     //gl_Position = (projectionMatrix * viewMatrix * modelMatrix) * vec4(position, 1.0);
     //The above enclosed brackets with the MVP matricies are all in u_modelMatrix already!
     gl_Position = u_modelMatrix * vec4(a_position, 1.0);
 }
-
-/* 
-//WebGL 2.0 Shader Implementation
-#version 300 es
-precision mediump float;
-
-// Attribute to hold vertex positions
-in vec3 a_position;
-
-// Uniforms
-uniform mat4 u_modelMatrix;  // Model transformation matrix
-uniform vec2 u_resolution;   // Resolution of the canvas
-
-// Pass to fragment shader
-void main() {
-    // Transform the vertex position by the model matrix
-    vec4 transformedPosition = u_modelMatrix * vec4(a_position, 1.0);
-    
-    // Convert the transformed position to clip space
-    // Clip space is from (-1, 1) in both x and y, so we normalize the positions
-    vec2 clipSpace = (transformedPosition.xy / u_resolution) * 2.0 - 1.0;
-    
-    // Output the position in clip space
-    gl_Position = vec4(clipSpace, 0.0, 1.0);
-}
-
-// WebGL 1.0 Shader Implementation
-attribute vec3 a_position;
-uniform mat4 u_modelMatrix;
-uniform vec2 u_resolution;
-
-void main() {
-    vec4 transformedPosition = u_modelMatrix * vec4(a_position, 1.0);
-    vec2 clipSpace = (transformedPosition.xy / u_resolution) * 2.0 - 1.0;
-    gl_Position = vec4(clipSpace, 0.0, 1.0);
-}
-*/
-
